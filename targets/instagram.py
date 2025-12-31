8495386adb9ea833a20ed5508f6e975adc4e8a26
Indrayaza Z <zainuddinindrayaza@gmail.com>
2025-12-31 12:23:26 +0800

    Add Instagram API payload and config functions

---
+++ targets/instagram.py
@@ -0,0 +1,13 @@
+from core.headers import HeaderGenerator
+
+def get_ig_payload(username, password):
+    return {
+        "params": f'{{"client_input_params":{{"username":"{username}","password":"{password}"}}}}',
+        "bk_client_context": '{"bloks_version":"..."}'
+    }
+
+def get_config():
+    return {
+        "url": "https://i.instagram.com/api/v1/accounts/login/",
+        "platform": "instagram"
+    }
