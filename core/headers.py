f22b8b1041785439a8051cd087cde17d493e36aa
Indrayaza Z <zainuddinindrayaza@gmail.com>
2025-12-31 12:27:36 +0800

    Add Connection header to base headers

--- core/headers.py
+++ core/headers.py
@@ -5,20 +5,35 @@ class HeaderGenerator:
     @staticmethod
     def get_headers(platform):
         ua = UserAgent()
-        base_headers = {"User-Agent": ua.random, "Accept-Language": "en-US,en;q=0.9"}
+        base_headers = {
+            "User-Agent": ua.random,
+            "Accept-Language": "en-US,en;q=0.9",
+            "Connection": "keep-alive"
+        }
         
         configs = {
             "instagram": {
                 "X-IG-App-ID": "936619743392459",
+                "X-IG-WWW-Claim": "0",
+                "Content-Type": "application/x-www-form-urlencoded"
+            },
+            "threads": {
+                "X-IG-App-ID": "238280545333740", # App ID Threads berbeda
                 "X-ASBD-ID": "129477",
                 "Content-Type": "application/x-www-form-urlencoded"
             },
             "facebook": {
                 "X-FB-LSD": "AVpHp_0E",
-                "X-ASBD-ID": "129477"
+                "Content-Type": "application/x-www-form-urlencoded",
+                "Referer": "https://m.facebook.com/"
+            },
+            "gmail": {
+                "Content-Type": "application/json",
+                "X-Requested-With": "XMLHttpRequest" # Simulasi AJAX request
             },
             "web_generic": {
-                "Referer": "https://google.com"
+                "Cache-Control": "max-age=0",
+                "Upgrade-Insecure-Requests": "1"
             }
         }
         return {**base_headers, **configs.get(platform, {})}
