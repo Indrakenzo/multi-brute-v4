ca31348b8dd9cb5a8910aeccdfd883d4d9d11780
Indrayaza Z <zainuddinindrayaza@gmail.com>
2025-12-31 17:49:07 +0800

    Update instagram.py

--- targets/instagram.py
+++ targets/instagram.py
@@ -1,9 +1,15 @@
-from core.headers import HeaderGenerator
+def get_config():
+    return {
+        "url": "https://i.instagram.com/api/v1/accounts/login/",
+        "platform": "instagram"
+    }
 
-def get_ig_payload(username, password):
+# [FIX] Nama fungsi diubah dari 'get_ig_payload' menjadi 'get_payload'
+# agar dikenali oleh main.py
+def get_payload(username, password):
     return {
         "params": f'{{"client_input_params":{{"username":"{username}","password":"{password}"}}}}',
-        "bk_client_context": '{"bloks_version":"..."}'
+        "bk_client_context": '{"bloks_version":"85f67073f15598696803272995250493608144081395804550742514120286d5"}'
     }
 
 def get_config():
